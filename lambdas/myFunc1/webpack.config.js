120b1d1e8555049b1f8413a74f4df4d1b1833eee
Dan <dfrasca80@gmail.com>
2020-08-24 05:16:23 +0000

    webpack.config.js for func1

---
+++ lambdas/myFunc1/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require('webpack');
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+const ZipPlugin = require('zip-webpack-plugin');
+
+module.exports = {
+  entry: './src/handler.ts',
+  mode: "production",
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json']
+  },
+  externals: [{
+    'aws-sdk': 'commonjs aws-sdk'
+  }],
+  plugins: [
+    new LodashModuleReplacementPlugin,
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new ZipPlugin({
+      filename: `func1.zip`
+    })
+  ],
+  output: {
+    filename: 'func1.js',
+    path: path.resolve(__dirname, 'resources'),
+    libraryTarget: 'commonjs2'
+  }
+};
